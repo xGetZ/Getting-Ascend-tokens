4604841b9b3856f32d88c3e78ca2ca280abb9ec0
xGetZ <112335261+xGetZ@users.noreply.github.com>
2023-02-13 23:06:17 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+string1 = "GetZ import your key:"
+string2 = "sd3cf45hdfsg6"
+string3 = "*GetZ clicks Register button*"
+string4 = "*ERROR 404 - SUNSET ON YOUR ASS *"
+
+Ascend_cracked_license = string1 + string2
+print(Ascend_cracked_license)
