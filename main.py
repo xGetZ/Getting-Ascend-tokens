4d312ab8aa929043d6c3b8898097b2dac3216ddb
xGetZ <112335261+xGetZ@users.noreply.github.com>
2023-02-13 23:07:20 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ string2 = "sd3cf45hdfsg6"
 string3 = "*GetZ clicks Register button*"
 string4 = "*ERROR 404 - SUNSET ON YOUR ASS *"
 
-Ascend_cracked_license = string1 + string2
+Ascend_cracked_license = string1 + string2 + string3 + string4
 print(Ascend_cracked_license)
